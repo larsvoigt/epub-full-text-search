573c6b960555a833a4ce3abd967bfb3a7cda6a5a
Lars Voigt <lars.voigt@dzb.de>
2016-05-31 16:04:53 +0200

    - final first version of teaser feature

--- src/Teaser.js
+++ src/Teaser.js
@@ -1,4 +1,16 @@
-const START_TAG = '<b class=&#39;hit&#39;>';
+'use strict'
+
+/**
+ * Module dependencies.
+ * @private
+ */
+const _ = require('lodash');
+
+/**
+ * Module variables.
+ * @private
+ */
+const START_TAG = "<b class='hit'>";
 const END_TAG = '</b>';
 
 exports.get = function (element) {
@@ -6,14 +18,45 @@ exports.get = function (element) {
     var textNode = element.textNode;
     var text = textNode.parent().contents();
 
-    if (text[0].type === 'text')
-        console.log("Text:   " + tagQuery(text[0].data, element.range.startOffset, element.range.endOffset));
+    if (text[0].type === 'text') {
+        //console.log("Text:   " + tagQuery(text[0].data, element.range.startOffset, element.range.endOffset));
+        text = tagQuery(text[0].data, element.range.startOffset, element.range.endOffset);
+        return escapeHtml(text);
+
+    }
+    else
+        return "";
 };
 
 
-function tagQuery(value, start, end) {
+function tagQuery(orgText, start, end) {
 
-    return value.substring(0, start) + START_TAG +
-        value.substring(start, end) + END_TAG +
-        value.substring(end);
+    var text = trim(orgText.substring(0, start), 10) + START_TAG +
+        orgText.substring(start, end) + END_TAG +
+        trim(orgText.substring(end), 10);
+
+    // replace linebreaks 
+    text = text.replace(/[\r\n]/g, ' ');
+    // remove multi whitespace
+    text = text.replace(/\s+/g, ' ');
+
+    return text;
+}
+
+function trim(text, count) {
+
+    const words = text.split(' ');
+
+    return (_.takeRight(words, count)).join(' ');
+}
+
+
+function escapeHtml(unsafe) {
+    //console.log(unsafe);
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
 }
